c5f3b08cc3b3e335ad004dcc2970eec5cb0f98ae
AndrzejR <andrzej.ruszczewski@gmail.com>
2015-05-31 12:43:42 +0200

    median

--- stats.py
+++ stats.py
@@ -3,16 +3,16 @@
 def mean(*args):
     """Returns the mean of args.
 
-    *args -- sequence to unpack, or separate numerical arguments
+    *args -- sequence to unpack or separate numerical arguments
     >>> mean(1,2,3)
     2.0
-    >>> mean(*[3,4,8])
-    5.0
+    >>> mean(*[-3,-4,-8])
+    -5.0
     >>> mean(*range(1001))
     500.0
     >>> mean()
     Traceback (most recent call last):
-    TypeError: *args cannot be empty
+    TypeError: needs at least one argument
     >>> mean('string')
     Traceback (most recent call last):
     TypeError: unsupported operand type(s) for +=: 'int' and 'str'
@@ -23,8 +23,31 @@ def mean(*args):
             running_sum += x
         return running_sum/len(args)
     except ZeroDivisionError:
-        raise TypeError("*args cannot be empty")
+        raise TypeError("needs at least one argument")
 
+def median(*args):
+    """Returns the median of args.
+
+    *args -- sequence to unpack or separate numerical arguments
+    >>> median(0,2,3,10)
+    2.5
+    >>> median(13,42,666)
+    42
+    >>> median('a string', 'string', 'a string')
+    'a string'
+    >>> median()
+    Traceback (most recent call last):
+    TypeError: needs at least one argument
+    """
+    try:
+        args = sorted(list(args))
+        n = len(args)
+        if n%2==0:
+            return (args[(n//2)-1]+args[n//2])/2
+        else:
+            return args[n//2]
+    except IndexError:
+        raise TypeError("needs at least one argument")
 
 if __name__ == "__main__":
     import doctest
